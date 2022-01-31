4a6caab64a0eaffeb81ac2ae99cf5c7cca352bf9
gabo <gabrielbarragan07@gmail.com>
2022-01-30 21:17:16 -0500

    add example env file

--- server/control_sedes/control_sedes/settings.py
+++ server/control_sedes/control_sedes/settings.py
@@ -58,7 +58,7 @@ AUTH_USER_MODEL = 'users.Account'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gabrielrondondiplomado@gmail.com'
+EMAIL_HOST_USER = env('USER_EMAIL')
 EMAIL_HOST_PASSWORD = env('USER_EMAIL_PASSWORD')
 EMAIL_USE_TLS = True
 
